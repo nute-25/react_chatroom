9842aeddcac9eb085a3ea092e5b88799f4d0619c
Nolwenn <nolwenn.poilleux@gmail.com>
2019-02-01 10:49:56 -0500

    send new message to database and display it, login/logout user

--- src/components/Chatroom.js
+++ src/components/Chatroom.js
@@ -8,7 +8,9 @@ class Chatroom extends Component {
         super();
         this.state = {
             number: [2,5,7],
-            firebase_tab: []
+            firebase_tab: [],
+            user: false,
+            msg: ''
         };
     }
     // se met à jour regulièrement dès qu'il y a modif
@@ -22,11 +24,14 @@ class Chatroom extends Component {
                     })
                 }
 
+                <button onClick={this.login.bind(this)}>Se connecter !</button>
+                { (this.state.user) ? <button onClick={this.logout.bind(this)}>Se déconnecter !</button> : '' }
+
                 <ul>
                     {
                         this.state.firebase_tab.map((elem) => {
                             return (
-                                <li>
+                                <li key={elem.ts}>
                                     { elem.ts } - { elem.displayName } :
                                     <p> { elem.message }</p>
                                 </li>
@@ -35,13 +40,19 @@ class Chatroom extends Component {
                     }
                 </ul>
 
-                <form>
-                    <input type="text" placeholder="Saisissez votre message"/>
-                    <input type="submit" value="Envoyer"/>
-                </form>
+                { (this.state.user)
+                    ? <form onSubmit={this.handleSubmit.bind(this)}>
+                        <input type="text" onChange={ this.handleChange.bind(this) } placeholder="Saisissez votre message"/>
+                        <input type="submit" value="Envoyer"/>
+                      </form>
+                    : ''
+                }
+
+
             </div>
         );
     }
+    // dans le mounted a lieu les changements d'état
     componentDidMount() {
         // ajout d'un elem à l'objet state
         this.setState({
@@ -64,6 +75,53 @@ class Chatroom extends Component {
                 })
             }
         });
+        firebase.auth().onAuthStateChanged(user => {
+            if (user) {
+                // Utilisateur connecté.
+                this.setState( {
+                    user: user
+                })
+            } else {
+                // Aucun utilisateur connecté.
+                this.setState( {
+                    user: false
+                })
+            }
+        });
+    }
+    // qd le texte saisi par l'utilisateur change
+    handleChange (event) {
+        this.setState({
+            msg: event.target.value
+        })
+    }
+
+    // qd l'utilisateur valide son message via le btn
+    handleSubmit(event) {
+        console.log('test', event)
+        event && event.preventDefault();
+        if (this.state.user) {
+            console.log('test2')
+            let entry = {
+                ts: new Date().getTime(),
+                uid: this.state.user.uid,
+                displayName: this.state.user.displayName,
+                message: this.state.msg
+            }
+            firebase.database().ref('messages/').push(entry)
+        }
+
+
+    }
+
+    login () {
+        let googleAuthProvider = new firebase.auth.GoogleAuthProvider()
+        googleAuthProvider.addScope('https://www.googleapis.com/auth/plus.login')
+        //firebase.auth().languageCode = 'fr'
+        firebase.auth().signInWithPopup(googleAuthProvider)
+    }
+    logout () {
+        firebase.auth().signOut()
     }
 }
 
